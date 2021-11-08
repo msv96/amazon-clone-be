e129deb01fd2aa46d39ccb244c1d49448de59dcf
Srinivasan M <msrini1100@gmail.com>
2021-11-08 18:45:52 +0530

    8-11 done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var dotenv = require("dotenv");
+var mongo = require("mongodb");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+dotenv.config();
+const mongoClient = mongo.MongoClient;
+const url = process.env.MONGODB;
+
+router.get("/", async function (req, res, next) {
+	try {
+		let client = await mongoClient.connect(url);
+
+		let db = client.db("amazon");
+
+		let data = await db.collection("products").find().toArray();
+
+		client.close();
+
+		res.json(data);
+	} catch (error) {
+		res.status(404).json({
+			message: "Something went wrong",
+		});
+	}
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/order", async function (req, res, next) {
+	try {
+		let client = await mongoClient.connect(url);
+
+		let db = client.db("amazon");
+
+		let data = await db.collection("users").insertOne(req.body);
+
+		await client.close();
+
+		res.json({
+			message: "User Registered",
+			id: data._id,
+			code: true,
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: "Something went wrong",
+			code: false,
+		});
+	}
 });
 
 module.exports = router;
